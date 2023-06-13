99bb330b205d449faac1e3c1a9891a774a55c8d1
emrahyumuk <emrahyumuk@gmail.com>
2023-06-13 10:03:03 +0300

    Fixed crash when calling GKLocalPlayer_FetchItems

--- plug-ins/Apple.GameKit/Apple.GameKit_Unity/Assets/Apple.GameKit/Source/GKIdentityVerificationResponse.cs
+++ plug-ins/Apple.GameKit/Apple.GameKit_Unity/Assets/Apple.GameKit/Source/GKIdentityVerificationResponse.cs
@@ -3,44 +3,48 @@ using System.Runtime.InteropServices;
 
 namespace Apple.GameKit.Players
 {
-    [StructLayout(LayoutKind.Sequential)]
-    public struct GKIdentityVerificationResponse
+  [StructLayout(LayoutKind.Sequential)]
+  public struct GKIdentityVerificationResponse
+  {
+    /// <summary>
+    /// The URL for the public encryption key.
+    /// </summary>
+    //public string PublicKeyUrl;
+    public string PublicKeyUrl;
+
+    /// <summary>
+    /// The verification signature data that GameKit generates.
+    /// </summary>
+    public byte[] Signature;
+
+    /// <summary>
+    /// A random NSString that GameKit uses to compute the hash and randomize it.
+    /// </summary>
+    public byte[] Salt;
+
+    /// <summary>
+    /// The signatureâ€™s creation date and time.
+    /// </summary>
+    public ulong Timestamp;
+
+    internal GKIdentityVerificationResponse(ulong timestamp,
+        IntPtr publicKeyUrl, int publicKeyUrlLength,
+        IntPtr signature, int signatureLength,
+        IntPtr salt, int saltLength)
     {
-        /// <summary>
-        /// The URL for the public encryption key.
-        /// </summary>
-        public string PublicKeyUrl;
-        internal IntPtr Signature;
-        internal int SignatureLength;
-        internal IntPtr Salt;
-        internal int SaltLength;
-        /// <summary>
-        /// The signatureâ€™s creation date and time.
-        /// </summary>
-        public ulong Timestamp;
+      Timestamp = timestamp;
 
-        /// <summary>
-        /// The verification signature data that GameKit generates.
-        /// </summary>
-        /// <returns></returns>
-        public byte[] GetSignature()
-        {
-            var signature = new byte[SignatureLength];
-            Marshal.Copy(Signature, signature, 0, SignatureLength);
+      var publicKeyUrlBytes = new byte[publicKeyUrlLength];
+      Marshal.Copy(publicKeyUrl, publicKeyUrlBytes, 0, publicKeyUrlLength);
+      PublicKeyUrl = System.Text.Encoding.UTF8.GetString(publicKeyUrlBytes);
 
-            return signature;
-        }
+      Signature = new byte[signatureLength];
+      Marshal.Copy(signature, Signature, 0, signatureLength);
 
-        /// <summary>
-        /// A random NSString that GameKit uses to compute the hash and randomize it.
-        /// </summary>
-        /// <returns></returns>
-        public byte[] GetSalt()
-        {
-            var salt = new byte[SaltLength];
-            Marshal.Copy(Salt, salt, 0, SaltLength);
+      Salt = new byte[saltLength];
+      Marshal.Copy(salt, Salt, 0, saltLength);
 
-            return salt;
-        }
     }
+
+  }
 }
